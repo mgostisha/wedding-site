e6c5a3c35fcc97316f068a0b33336021b8853850
Martin Gostisha <martin@flipgrid.com>
2018-10-02 21:56:38 -0500

    Menu fixes, content adjustmnets

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('menu-btn').addEventListener('click', function() {
     document.getElementById('menu').classList.toggle('open');
+    document.getElementById('menu-btn').classList.toggle('open');
   });
 
   ['wedding', 'accomodations', 'registry', 'adventures'].forEach(function (key) {
     document.getElementById('menu-content-' + key).addEventListener('click', function() {
       document.getElementById(key).scrollIntoView({ behavior: 'smooth' });
       document.getElementById('menu').classList.toggle('open');
+      document.getElementById('menu-btn').classList.toggle('open');
     });
   });
 });
